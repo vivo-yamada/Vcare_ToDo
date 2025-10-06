2f9131f59e8823d4a18facde90a0a84270042fec
kota_yamada <k_yamada@vivo-system.co.jp>
2025-10-06 16:49:18 +0900

    詳細画面の日付表示不具合の修正

--- VcareTodo/obj/Debug/net9.0/VcareTodo.AssemblyInfo.cs
+++ VcareTodo/obj/Debug/net9.0/VcareTodo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990be10975cb89a952f88d406c066e4154782ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a8907835a014056617bda184e57bc127f0dc0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
