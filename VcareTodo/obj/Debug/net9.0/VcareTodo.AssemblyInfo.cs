61e3511e50486ce8111dd115cb2d004f3597d7a1
kota_yamada <k_yamada@vivo-system.co.jp>
2025-11-24 19:31:03 +0900

    時間スロットごとのハイライトに修正

--- VcareTodo/obj/Debug/net9.0/VcareTodo.AssemblyInfo.cs
+++ VcareTodo/obj/Debug/net9.0/VcareTodo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9131f59e8823d4a18facde90a0a84270042fec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94010249cf90af4f06b31c4090d77b58f5c4993")]
 [assembly: System.Reflection.AssemblyProductAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VcareTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
